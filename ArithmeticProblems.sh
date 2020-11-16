cfb0ff49bc2fe265cc95e4230b06a471975f6ab0
abh224272 <abh224272@gmail.com>
2020-11-16 17:13:48 +0530

    Added: Read Command to accept 3 Numbers

--- ArithmeticProblems.sh
+++ ArithmeticProblems.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-echo "Welcome of the Program being Sovled in the master Branch"
+echo "*******Welcome of the Program being Sovled in the master Branch*******"
+echo "Enter Three Numbers to compute Arithmetic Operations"
+read first
+read second
+read third
