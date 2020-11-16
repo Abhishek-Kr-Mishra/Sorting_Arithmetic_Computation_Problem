7e216f13c6fa18adec232cbd957c5678d6a62f3a
abh224272 <abh224272@gmail.com>
2020-11-16 17:40:25 +0530

    Added: First Arithmetic Operation

--- ArithmeticProblems.sh
+++ ArithmeticProblems.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-echo "*******Welcome of the Program being Sovled in the master Branch*******"
+	echo "*******Welcome of the Program being Sovled in the master Branch*******"
+
 echo "Enter Three Numbers to compute Arithmetic Operations"
-read first
-read second
-read third
+read a
+read b
+read c
+
+UC1=$(( $a + ($b*$c) ))
+echo "Value of UC1 is: $UC1"
